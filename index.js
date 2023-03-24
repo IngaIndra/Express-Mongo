7af965569ce787919b87db2cff15fefa55a5327c
Indra.U <indra.ulziibadrakh@example.com>
2023-03-24 16:42:02 +0800

    image uploader

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
 import express from "express";
 import "./configs/mongoose-config.js";
 import userRouter from "./routes/users.router.js";
+import todoCategoriesRouter from "./routes/todoCategories-router.js";
+import todoRouter from "./routes/todo-router.js";
 import multer from "multer";
 import { nanoid } from "nanoid";
 import cloudinary from "cloudinary";
-
-cloudinary.config({
-  cloud_name: "df8az1u5k",
-  api_key: "792374388872524",
-  api_secret: "9TvZ89y4BmTTvNM5TY1Gyiasqtc",
-});
+import cors from "cors";
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    // const mimeType = file.mimetype;
-    // console.log("mimeType:", mimeType);
-    // let dest = "";
-    // if (mimeType.includes("image")) {
-    //   dest += "/images";
-    // } else if (mimeType.includes("video")) {
-    //   dest += "/videos";
-    // } else if (mimeType.includes("pdf")) {
-    //   dest += "/docs";
-    // } else if (mimeType.includes("audio")) {
-    //   dest += "/audios";
-    // } else {
-    //   dest += "/others";
-    // }
     cb(null, "/tmp");
   },
   filename: (req, file, cb) => {
@@ -57,9 +40,9 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage,
-  // limits: {
-  //   fileSize: 10 * 1024 * 1024 * 1024,
-  // },
+  limits: {
+    fileSize: 10 * 1024 * 1024 * 1024,
+  },
   fileFilter: (req, file, cb) => {
     if (file.mimetype.includes("image") || file.mimetype.includes("video")) {
       cb(null, true);
@@ -70,22 +53,24 @@ const upload = multer({
 });
 
 const PORT = 8080;
-
 const app = express();
+
 app.use(express.json());
 
 app.use("/api/users", userRouter);
+app.use("/api/todo/categories", todoCategoriesRouter);
+app.use("/api/todo", todoRouter);
+app.use(cors());
 
-app.post("/files", upload.single("image"), (req, res) => {
-  // res.send(req.file);
-  const uploadedFile = cloudinary.v2.uploader
-    .upload(req.file.path)
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((e) => {
-      console.log("uploadedFile", e);
-    });
+cloudinary.config({
+  cloud_name: "df8az1u5k",
+  api_key: "792374388872524",
+  api_secret: "9TvZ89y4BmTTvNM5TY1Gyiasqtc",
+});
+
+app.post("/files", upload.single("image"), async (req, res) => {
+  const uploadedFile = await cloudinary.v2.uploader.upload(req.file.path);
+  res.json(uploadedFile);
 });
 
 app.use("/uploads", express.static("uploads"));
